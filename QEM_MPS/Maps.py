29bac00e5c685903fef56ee64560ee2e71080d14
WeiguoMA <david.mawg@gmail.com>
2023-06-07 17:33:21 +0800

    Adopted to SuperOperator.oy

--- QEM_MPS/Maps.py
+++ QEM_MPS/Maps.py
@@ -6,11 +6,16 @@ Contact: weiguo.m@iphy.ac.cn
 import torch as tc
 import copy
 import tensornetwork as tn
-from Library.tools import EdgeName2AxisName
+from superOperator import SuperOperator
+from Library.tools import EdgeName2AxisName, generate_random_string_without_duplicate
+
+tn.set_default_backend("pytorch")
+
 
 class UPMPO(object):
 	""" get the uPMPO from uMPO-shapeLike"""
-	def __init__(self, uMPO: list[tn.AbstractNode]):
+
+	def __init__(self, uMPO: list[tn.AbstractNode] or SuperOperator):
 		self.uPMPO = copy.deepcopy(uMPO)
 		self._substitute2RandomTensors()
 
@@ -18,14 +23,15 @@ class UPMPO(object):
 		""" substitute the UTensorMPO with random tensors """
 		for _i, _node in enumerate(self.uPMPO):
 			_shape = _node.shape
-			_node.name = f'UP_{_i}'
-			_node.tensor = tc.randn(size=_shape)
+			_node.name = _node.name.replace('U', 'UP')
+			_node.tensor = tc.randn(size=_shape, dtype=tc.complex128)
 
 
 class Maps(object):
 	def __init__(self, superOperatorMPO: list[tn.AbstractNode], uPMPO: list[tn.AbstractNode]):
 		self.uMPO_Ori = superOperatorMPO
 		self.uPMPO_Ori = uPMPO
+		self.qubitNum = int(len(self.uMPO_Ori) / 2)
 
 		self.uDMPO_Ori = self._getConjugate(self.uMPO_Ori)
 		self.uPDMPO_Ori = self._getConjugate(self.uPMPO_Ori)
@@ -48,19 +54,55 @@ class Maps(object):
 		EdgeName2AxisName(MPO)
 		return MPO
 
+	@staticmethod
+	def findElementsInListWithOrder(inList, orders: list[str]):
+		idxList = []
+		for element in orders:
+			idxList.append(inList.index(element))
+		return idxList
+
+	def _reorderTensor(self, tensor: tc.Tensor, _NodeAxisName: list[str], _order: list[str]) -> tc.Tensor:
+		for _i, _name in enumerate(_NodeAxisName):
+			if 'bond' in _name and 'Dagger' not in _name:
+				_NodeAxisName[_i] = 'Dagger' + _NodeAxisName[_i]
+			elif 'tr' in _name:
+				_NodeAxisName[_i] = [_name_ for _name_ in _order if 'tr' in _name_][0]  # Hard-fix may cause problem
+			elif 'tr' not in _name and 'bond' not in _name:
+				_NodeAxisName[_i] = [_name_ for _name_ in _order if 'tr' not in _name_ and 'bond' not in _name_][0]
+			else:
+				pass
+
+		_order = self.findElementsInListWithOrder(_order, _NodeAxisName)
+		_randomString = generate_random_string_without_duplicate(len(_order))
+		_reorderString = ''.join([_randomString[_i] for _i in _order])
+
+		_tensor = tc.einsum(f'{_randomString} -> {_reorderString}', tensor)
+
+		return _tensor
+
 	def MMAP(self):
 		_uMPO, _uPMPO = copy.deepcopy(self.uMPO_Ori), copy.deepcopy(self.uPMPO_Ori)
 		_uDMPO, _uPDMPO = copy.deepcopy(self.uDMPO_Ori), copy.deepcopy(self.uPDMPO_Ori)
 
-		for _num in range(len(_uMPO)):
+		for _num in reversed(range(self.qubitNum)):
+			# Connect u -> u^\dagger -- D
+			tn.connect(_uMPO[self.qubitNum-1-_num][f'Dinner_{_num}'], _uDMPO[self.qubitNum-1-_num][f'Dphysics_{_num}'], name=f'Du_uD_{_num}')
+			# Connect u' -> u -- D
+			tn.connect(_uPMPO[self.qubitNum-1-_num][f'Dinner_{_num}'], _uMPO[self.qubitNum-1-_num][f'Dphysics_{_num}'], name=f'DuP_u_{_num}')
+			# Connect u^\dagger -> u'^\dagger -- D
+			tn.connect(_uDMPO[self.qubitNum-1-_num][f'Dinner_{_num}'], _uPDMPO[self.qubitNum-1-_num][f'Dphysics_{_num}'], name=f'DuD_uPD_{_num}')
+			# Connect u' -> u'^\dagger -- D // TRACE Edge
+			tn.connect(_uPMPO[self.qubitNum-1-_num][f'Dphysics_{_num}'], _uPDMPO[self.qubitNum-1-_num][f'Dinner_{_num}'], name=f'DuP_uPD_tr_{_num}')
+
+		for _num in range(self.qubitNum):
 			# Connect u -> u^\dagger
-			tn.connect(_uMPO[_num][f'inner_{_num}'], _uDMPO[_num][f'physics_{_num}'], name=f'u_uD_{_num}')
+			tn.connect(_uMPO[self.qubitNum+_num][f'inner_{_num}'], _uDMPO[self.qubitNum+_num][f'physics_{_num}'], name=f'u_uD_{_num}')
 			# Connect u' -> u
-			tn.connect(_uPMPO[_num][f'inner_{_num}'], _uMPO[_num][f'physics_{_num}'], name=f'uP_u_{_num}')
+			tn.connect(_uPMPO[self.qubitNum+_num][f'inner_{_num}'], _uMPO[self.qubitNum+_num][f'physics_{_num}'], name=f'uP_u_{_num}')
 			# Connect u^\dagger -> u'^\dagger
-			tn.connect(_uDMPO[_num][f'inner_{_num}'], _uPDMPO[_num][f'physics_{_num}'], name=f'uD_uPD_{_num}')
+			tn.connect(_uDMPO[self.qubitNum+_num][f'inner_{_num}'], _uPDMPO[self.qubitNum+_num][f'physics_{_num}'], name=f'uD_uPD_{_num}')
 			# Connect u' -> u'^\dagger // TRACE Edge
-			tn.connect(_uPMPO[_num][f'physics_{_num}'], _uPDMPO[_num][f'inner_{_num}'], name=f'uP_uPD_tr_{_num}')
+			tn.connect(_uPMPO[self.qubitNum+_num][f'physics_{_num}'], _uPDMPO[self.qubitNum+_num][f'inner_{_num}'], name=f'uP_uPD_tr_{_num}')
 
 		for _list in [_uMPO, _uPMPO, _uDMPO, _uPDMPO]:
 			EdgeName2AxisName(_list)
@@ -70,19 +112,32 @@ class Maps(object):
 	def NMAP(self):
 		uDMPO, uPDMPO = copy.deepcopy(self.uDMPO_Ori), copy.deepcopy(self.uPDMPO_Ori)
 
-		for _num in range(len(uDMPO)):
+		for _num in reversed(range(self.qubitNum)):
+			# Connect u^\dagger -> u'^\dagger -- D
+			tn.connect(uDMPO[self.qubitNum-1-_num][f'Dinner_{_num}'], uPDMPO[self.qubitNum-1-_num][f'Dphysics_{_num}'], name=f'DuD_uPD_{_num}')
+			# Connect u' -> u'^\dagger -- D // TRACE Edge
+			tn.connect(uPDMPO[self.qubitNum-1-_num][f'Dinner_{_num}'], uDMPO[self.qubitNum-1-_num][f'Dphysics_{_num}'], name=f'DuD_uPD_tr_{_num}')
+
+		for _num in range(self.qubitNum):
 			# Connect u^\dagger -> u'^\dagger
-			tn.connect(uDMPO[_num][f'inner_{_num}'], uPDMPO[_num][f'physics_{_num}'], name=f'uD_uPD_{_num}')
+			tn.connect(uDMPO[self.qubitNum+_num][f'inner_{_num}'], uPDMPO[self.qubitNum+_num][f'physics_{_num}'], name=f'uD_uPD_{_num}')
 			# Connect u' -> u'^\dagger // TRACE Edge
-			tn.connect(uPDMPO[_num][f'inner_{_num}'], uDMPO[_num][f'physics_{_num}'], name=f'uD_uPD_tr_{_num}')
+			tn.connect(uPDMPO[self.qubitNum+_num][f'inner_{_num}'], uDMPO[self.qubitNum+_num][f'physics_{_num}'], name=f'uD_uPD_tr_{_num}')
 
 		for _list in [uDMPO, uPDMPO]:
 			EdgeName2AxisName(_list)
 		NMAP = {'uDMPO': uDMPO, 'uPDMPO': uPDMPO}
 		self.NMap = NMAP
 
-	def updateMMap(self):
-		pass
+	def updateMMap(self, bTensor: tc.Tensor, BNum: int, fixedNameOrder: list[str]):
+		self.MMap['uPMPO'][BNum].tensor = self._reorderTensor(bTensor, self.MMap['uPMPO'][BNum].axis_names,
+		                                                      fixedNameOrder)
+		self.MMap['uPDMPO'][BNum].tensor = self._reorderTensor(bTensor.conj(), self.MMap['uPDMPO'][BNum].axis_names,
+		                                                       fixedNameOrder)
+		EdgeName2AxisName([self.MMap['uPMPO'][BNum], self.MMap['uPDMPO'][BNum]])    # Hard-reset for fixing prob in _re
 
-	def updateNMap(self):
-		pass
+	def updateNMap(self, bTensor: tc.Tensor, BNum: int, fixedNameOrder: list[str]):
+		# print('NMap uPDMPO Shape:', self.NMap['uPDMPO'][BNum].axis_names)
+		self.NMap['uPDMPO'][BNum].tensor = self._reorderTensor(bTensor.conj(), self.NMap['uPDMPO'][BNum].axis_names,
+		                                                       fixedNameOrder)
+		EdgeName2AxisName([self.NMap['uPDMPO'][BNum]])  # Hard-reset for fixing prob in _reorderTensor
