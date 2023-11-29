3cd8c339544ec153412ab0972938cdf6449697a6
WeiguoMA <david.mawg@gmail.com>
2023-11-29 11:44:16 +0800

    Main.py for New Edition of Program.

--- Noisy quantum circuit with MPDO/main.py
+++ Noisy quantum circuit with MPDO/main.py
@@ -6,42 +6,43 @@ Contact: weiguo.m@iphy.ac.cn
 import tensornetwork as tn
 
 import Library.tools as tools
-from Library.ADCircuits import TensorCircuit
-from Library.AbstractGate import AbstractGate
-
-# # Ignore warnings from tensornetwork package when using pytorch backend for svd
-# warnings.filterwarnings("ignore")
-# # Bugs fixed in tensornetwork package, which is 'torch.svd, torch.qr' -> 'torch.linalg.svd, torch.linalg.qr'
+from Library.QuantumCircuit import TensorCircuit
 
 tn.set_default_backend("pytorch")
 
 # Basic information of circuit
 qnumber = 5
-ideal_circuit = False   # or True
-crossTalk = False    # or False
-noiseType = 'realNoise'		# or 'realNoise' or 'idealNoise'
-chiFilename = './data/chi/chi1.mat'
+ideal_circuit = False  # or True
+noiseType = 'realNoise'  # or 'realNoise' or 'idealNoise'
+
+chiFileNames = {
+    'CZ': {'01': './data/chi/czDefault.mat', '12': './data/chi/czDefault.mat', '23': './data/chi/czDefault.mat',
+           '34': './data/chi/czDefault.mat'},
+    'CP': {}
+}
 chi, kappa = 4, 4
 
 # Establish a quantum circuit
 circuit = TensorCircuit(qn=qnumber, ideal=ideal_circuit, noiseType=noiseType,
-                        chiFilename=chiFilename, crossTalk=crossTalk, chi=chi, kappa=kappa, chip='worst4Test', device='cpu')
+                        chiFileDict=chiFileNames, chi=chi, kappa=kappa, chip='worst4Test', device='cpu')
 
-circuit.add_gate(AbstractGate().h(), [0])
-circuit.add_gate(AbstractGate().cnot(), [0, 1])
-circuit.add_gate(AbstractGate().cnot(), [1, 2])
-circuit.add_gate(AbstractGate().cnot(), [2, 3])
-circuit.add_gate(AbstractGate(_lastTrunc=True).cnot(), [3, 4])
+circuit.h(0)
+circuit.cnot(0, 1)
+circuit.cnot(1, 2)
+circuit.cnot(2, 3)
+circuit.cnot(3, 4)
+
+# Set TensorNetwork Truncation
+circuit.truncate(True)
 
 print(circuit)
-
 
 # Generate an initial quantum state
 state = tools.create_ket0Series(qnumber)
 state = circuit(state, state_vector=False, reduced_index=[])
 
 # Calculate probability distribution
-prob_dict = tools.density2prob(state, tolerant=5e-4)
+prob_dict = tools.density2prob(state, tol=5e-4)  # Set _dict=False to return a np.array
 
 # plot probability distribution
 tools.plot_histogram(prob_dict, title=f'"{noiseType}" Probability Distribution')
